1de5c762d3c4c46d3b5396419cf30da2e072ab7e
Spid3r0 <utku_yazici@hotmail.com>
2022-11-22 21:53:42 +0300

    structing view and viewmodel

--- lib/data/database/database_provider.dart
+++ lib/data/database/database_provider.dart
@@ -3,7 +3,7 @@ import 'package:sqflite/sqflite.dart';
 import 'database_model.dart';
 
 abstract class DatabaseProvider<T extends DatabaseModel> {
-  String databaseName = "deneme.db";
+
 
   Future open();
 
